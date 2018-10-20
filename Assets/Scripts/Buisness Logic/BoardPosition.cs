408543bdbc79b0a37e0496a8a87a20cd170402b3
thepigeonfighter <georgefabish@gmail.com>
2018-10-20 16:37:29 -0400

    Overwrote the BoarPositon.Equals method

--- Assets/Scripts/Buisness Logic/BoardPosition.cs
+++ Assets/Scripts/Buisness Logic/BoardPosition.cs
@@ -13,4 +13,18 @@ public class BoardPosition{
 	public DateTime TimeSet{ get; set; }
 	public IPlayer Player{ get; set; }
 
+	public override bool Equals(object obj)
+	{
+		if(obj == null || !this.GetType().Equals(obj.GetType()))
+		{
+            return false;
+        }
+        BoardPosition position = (BoardPosition)obj;
+        return (position.XIndex == this.XIndex) && (position.YIndex == this.YIndex);
+    }
+	public override int GetHashCode()
+	{
+        return (XIndex << 2) ^ YIndex;
+    }
+
 }
